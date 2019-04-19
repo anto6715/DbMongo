e3d085eb8888af69142233e2382f2b9dcfaf50f1
Antonio <mariani.antonio@outlook.it>
2019-04-19 11:18:45 +0200

    Fix data format

--- src/main/java/it/unisalento/db/crud/DbMongo/WebConfig.java
+++ src/main/java/it/unisalento/db/crud/DbMongo/WebConfig.java
@@ -1,10 +1,12 @@
 package it.unisalento.db.crud.DbMongo;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -13,19 +15,18 @@ import org.springframework.web.servlet.view.JstlView;
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
-/*
-    @Bean
+    /*@Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
         viewResolver.setViewClass(JstlView.class);
         viewResolver.setPrefix("/templates/");
         viewResolver.setSuffix(".jsp");
         return viewResolver;
-    }
-*/
+    }*/
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedMethods("GET", "POST");
     }
+
 
 }
