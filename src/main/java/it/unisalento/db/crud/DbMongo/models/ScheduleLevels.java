e7cebf7b1d7e3040c7dde0ec3a40ee9e185cf4e0
Antonio <antonio.mariani@studenti.unisalento.it>
2019-05-25 18:06:22 +0200

    fix original level at max resolution

--- src/main/java/it/unisalento/db/crud/DbMongo/models/ScheduleLevels.java
+++ src/main/java/it/unisalento/db/crud/DbMongo/models/ScheduleLevels.java
@@ -35,16 +35,21 @@ public class ScheduleLevels implements Runnable {
         DB db = new MongoClient().getDB("demo");
         Jongo jongo = new Jongo(db);
         MongoCollection collection = jongo.getCollection("demo");
+        Iterator<Test> it;
 
 
 
 
-        Iterator<Test> it = collection.aggregate("{$match: {measureTimestamp.date : {$gte: #, $lt: # }}}", date_start,date_end)
+        if (zoom != 16) {
+             it = collection.aggregate("{$match: {measureTimestamp.date : {$gte: #, $lt: # }}}", date_start,date_end)
                 .and("{ $project : {lon_a: { $divide: [{ $trunc: { $multiply: ['$position.lon', "+approx+"]} }, "+approx+"]},lat_a: {$divide: [{$trunc: {$multiply: ['$position.lat', "+approx+"]}}, "+approx+"] }, lon : '$position.lon', lat: '$position.lat', leq: '$measurement.leq'}}")
                 .and("{$project : {_id: { $concat: [ { $toString: '$lon_a' } , '_' , { $toString: '$lat_a' } ] }, lon: 1,lat: 1, leq: 1}}")
                 .and("{$group : {_id: '$_id', lon: { $avg: '$lon' },lat: { $avg: '$lat' }, leq: { $avg: '$leq' }}}")
                 .and("{$project : {position: {lat: '$lat', lon: '$lon'},measurement: {leq: '$leq' },measureTimestamp: {date: #}}}", date_start)
                 .and("{$out: #}",collectionName).options(AggregationOptions.builder().allowDiskUse(true).build()).as(Test.class).iterator();
+        } else { it = collection.aggregate("{$match: {measureTimestamp.date : {$gte: #, $lt: # }}}", date_start,date_end)
+                .and("{$out: #}",collectionName).options(AggregationOptions.builder().allowDiskUse(true).build()).as(Test.class).iterator();
+        }
         int count = 0;
         while (it.hasNext()) {
             //tests.add(it.next());
